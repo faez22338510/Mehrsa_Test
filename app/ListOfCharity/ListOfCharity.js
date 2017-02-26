7cf702828105f16aae788b99dcf6c6133290e92c
faez22338510 <faez.ham@gmail.com>
2017-02-26 19:53:20 +0330

    Add files via upload

---
+++ app/ListOfCharity/ListOfCharity.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Button,
+    ScrollView
+} from 'react-native';
+
+class ListOfCharity extends Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <View><Text>Page LIST</Text></View>
+        )
+    }
+}
+
+module.exports = ListOfCharity;
