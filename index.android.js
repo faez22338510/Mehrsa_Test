b6f24421c9efa5a6ad9f3c8a24b6b250f641f877
faez22338510 <faez.ham@gmail.com>
2017-06-25 13:15:36 +0430

    add paypage

--- index.android.js
+++ index.android.js
@@ -4,72 +4,97 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
     AppRegistry,
     StyleSheet,
     Text,
     View,
-    ScrollView,
     Navigator,
-    BackAndroid
 } from 'react-native';
+
+
+var SplashPage = require('./app/Splash/Splash');
 var FirstPage = require('./app/FirstPage/firstPage');
-var CharityDetailPage = require('./app/CharityDetailes/CharityDetail');
+var CharityDetail = require('./app/CharityDetailes/DetailOfCharity');
+var PayPage = require('./app/PayPage/PayPage');
+var SliderDetail = require('./app/SliderDetail/SliderDetail');
 
 
-export default class Testing1 extends Component {
-
-    renderScene ( route, navigator ) {
+export default class MainProject extends Component {
+    renderScene(route, navigator) {
         var routeId = route.id;
         if (routeId === 0) {
+            return (
+                <SplashPage
+                    charitylist={route.listofCharity}
+                    navigator={navigator}
+                />
+            )
+        }
+        if (routeId === 1) {
             return (
                 <FirstPage
                     navigator={navigator}/>
             );
         }
-        if (routeId === 1) {
+        if (routeId === 2) {
             return (
-                <CharityDetailPage
+                <CharityDetail
+                    id = {route.orgId}
+                    navigator={navigator}/>
+            );
+        }
+        if (routeId === 3) {
+            return (
+                <SliderDetail
+                    id = {route.orgId}
+                    image = {route.image}
+                    navigator={navigator}
+                />
+            );
+        }
+        if (routeId === 4) {
+            return (
+                <PayPage
                     id = {route.orgId}
                     navigator={navigator}/>
             );
         }
     }
 
-
-  render() {
-    return (
-        <Navigator
-            initialRoute={{id: 0, orgId:0}}
-            renderScene={this.renderScene.bind(this)}
-            configureScene={(route) => {
+    render() {
+        return (
+            <Navigator
+                initialRoute={{id: 0, orgId:0}}
+                renderScene={this.renderScene.bind(this)}
+                configureScene={(route) => {
         if (route.sceneConfig) {
           return route.sceneConfig;
         }
-        return Navigator.SceneConfigs.VerticalDownSwipeJump;
+        return Navigator.SceneConfigs.FloatFromBottom;
       }}/>
-    );
-  }
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#e6e6e6',
-  },
-  rows:{
-    paddingTop:10,
-  } ,
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
 
-AppRegistry.registerComponent('Testing1', () => Testing1);
+AppRegistry.registerComponent('MainProject', () => MainProject);
