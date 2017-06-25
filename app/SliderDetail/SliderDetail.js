5db5de6533ba8d9c6f8f0dda1b011c4d57074241
faez22338510 <faez.ham@gmail.com>
2017-06-25 13:20:37 +0430

    Update SliderDetail.js

--- app/SliderDetail/SliderDetail.js
+++ app/SliderDetail/SliderDetail.js
@@ -32,23 +32,74 @@ BackAndroid.addEventListener('back', () => {
 class SliderDetail extends Component {
     constructor(props) {
         super(props);
-        _navigator  = this.props.navigator;
+        _navigator = this.props.navigator;
         this.state = {
-            id: props.id
+            id: props.id,
+            detail: {
+                "id": 0,
+                "title": "",
+                "stitle": "",
+                "description": "",
+                "btn_text": "",
+                "btn_action": 0,
+                "push": 0,
+                "slider": 1,
+                "slider_timer": 1,
+                "updated_at": "",
+                "assets": [
+                    {
+                        "id": 0,
+                        "type": "picture",
+                        "asset_url": "",
+                        "order": null
+                    }
+                ]
+            }
         }
+        this._callApi();
     }
 
-    callApi(){
-
+    _callApi() {
+        var url = "http://api.mehrsaa.ir/v1/slider/" + this.props.id;
+        fetch(url)
+            .then((response) => response.json())
+            .then((res) => {
+                if (res.meta.code == 200) {
+                    this.setState({
+                        detail: res.data,
+                    })
+                }
+            })
     }
 
     render() {
         return (
             <ScrollView>
-                <Text>SliderDetail</Text>
-                <Image source={{uri: this.props.image}}
-                       style={styles.SliderImage}/>
-
+                <View>
+                    <Image source={{uri: this.props.image}}
+                           style={styles.SliderImage}/>
+                </View>
+                <View>
+                    <View>
+                        <View>
+                            <Text>{this.state.detail.title}</Text>
+                        </View>
+                        <View>
+                            <Text>{this.state.detail.stitle}</Text>
+                        </View>
+                    </View>
+                    <View
+                    style={{width:200}}>
+                        <Button
+                            style={styles.btm_style}
+                            onPress={()=> null}
+                            title={this.state.detail.btn_text}
+                            />
+                    </View>
+                </View>
+                <View>
+                    <Text>{this.state.detail.description}</Text>
+                </View>
             </ScrollView>
         )
     }
@@ -56,9 +107,14 @@ class SliderDetail extends Component {
 }
 
 const styles = StyleSheet.create({
-    SliderImage:{
-        width:370,
-        height:240,
+    SliderImage: {
+        width: 370,
+        height: 240,
+    },
+    btm_style:{
+        width:20,
+        height:"auto",
+        paddingLeft:10,
     }
 });
 
